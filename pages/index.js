ddd7ccf87cd135cbaef79ca4edd701dd153467c2
HakanKavci <hackankavci0@gmail.com>
2023-05-03 19:59:45 +0300

    Layout component created and wrapped the _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique modi
@@ -14,7 +11,6 @@ export default function Home() {
         enim ipsam animi dolorum aperiam consequuntur?
       </p>
       <Link href="/ninjas">See Ninja Listing</Link>
-      <Footer />
     </div>
   );
 }
